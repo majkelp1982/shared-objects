3a370cedcc3b7f425ee9b241e5aea5b0dadd96f6
majkelp1982 <s.pomazanka@gmail.com>
2023-02-01 06:32:21 +0100

    initial release

---
+++ src/main/java/pl/smarthouse/sharedobjects/enums/Operation.java
@@ -0,0 +1,9 @@
+package pl.smarthouse.sharedobjects.enums;
+
+public enum Operation {
+  STANDBY,
+  AIR_EXCHANGE,
+  HUMIDITY_ALERT,
+  COOLING,
+  HEATING
+}
---
+++ src/main/java/pl/smarthouse/sharedobjects/enums/ZoneName.java
@@ -0,0 +1,16 @@
+package pl.smarthouse.sharedobjects.enums;
+
+public enum ZoneName {
+  SALON,
+  KUCHNIA,
+  BIURO,
+  LAZ_DOL,
+  PRZEDPOKOJ,
+  PRALNIA,
+  WARSZTAT,
+  RODZICE,
+  GARDEROBA,
+  NATALIA,
+  LAZ_GORA,
+  KAROLINA
+}
